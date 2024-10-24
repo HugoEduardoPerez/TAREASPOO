b389c0004e0c5903d54cbdd6e67ba95e44bb4746
HugoEduardoPerez <hugoperez2213@gmail.com>
2024-10-23 21:49:44 -0600

    Tarea 4 Unidad 3

--- Hospital/Hoospital/src/menu/MenuAdministrador.java
+++ Hospital/Hoospital/src/menu/MenuAdministrador.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//
 public class MenuAdministrador {
     private Scanner scanner = new Scanner(System.in);
     public int mostrarMenu() {
